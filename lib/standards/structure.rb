596c03900b0f8a206a722ca83659436416aefcb0
Josh Cheek <josh.cheek@gmail.com>
2014-09-04 15:09:31 -0600

    Structure is tested, implements equality, and adding a standard is actually correct
    accepts attribute hashes or instances of Standard
    increments non-provided ids
    raises errors on overlap
    appends to the list of standards

--- lib/standards/structure.rb
+++ lib/standards/structure.rb
@@ -2,9 +2,10 @@ require 'standards/standard'
 
 module Standards
   class Structure
-    attr_accessor :standards
-    def initialize(standards)
-      self.standards = standards
+    attr_reader :standards
+    def initialize(standards=[])
+      @standards = []
+      standards.each { |standard| add_standard standard }
     end
 
     def to_json
@@ -16,8 +17,28 @@ module Standards
     end
 
     def add_standard(standard_attributes)
-      Standard.new(standard_attributes)
-              .tap { |s| standards << s }
+      new_standard = standard_attributes
+      new_standard = Standard.new(standard_attributes) unless Standard === standard_attributes
+
+      new_standard.id ||= next_standard_id
+
+      if overlap = standards.find { |old_standard| old_standard.id == new_standard.id }
+        raise ArgumentError, "You provided the id #{new_standard.id.inspect}, which overlaps with #{overlap.inspect}"
+      end
+
+      standards << new_standard
+      new_standard
+    end
+
+    def ==(other)
+      return false unless self.class === other
+      standards == other.standards
+    end
+
+    private
+
+    def next_standard_id
+      (standards.map(&:id).max || 0).next
     end
   end
 end
---
+++ spec/structure_spec.rb
@@ -0,0 +1,90 @@
+require 'standards'
+
+RSpec.describe 'Structure' do
+  Standard  = Standards::Standard
+  Structure = Standards::Structure
+
+  it 'has a collection of standards which default to an empty array' do
+    structure = Structure.new
+    expect(structure.standards).to eq []
+
+    structure = Structure.new [Standard.new(id: 1)]
+    expect(structure.standards).to eq [Standard.new(id: 1)]
+  end
+
+  it 'initializes the standards via add_standards' do
+    structure = Structure.new [{id: 1}, Standard.new(id: 2)]
+    expect(structure.standards).to eq [Standard.new(id: 1), Standard.new(id: 2)]
+  end
+
+  it 'represents itself as a hash' do
+    structure = Structure.new [{id: 12}]
+    expect(structure.to_hash).to eq(standards: [ {id: 12, standard: '', tags: []} ])
+  end
+
+  context 'adding a standard' do
+    it 'returns the standard' do
+      structure = Structure.new
+      expect(structure.add_standard Standard.new(id: 10)).to eq Standard.new(id: 10)
+    end
+
+    it 'accepts attributes or instances of Standard' do
+      standard = Standard.new id: 12, standard: 'a', tags: ['b']
+
+      structure1 = Structure.new
+      structure2 = Structure.new
+      standard1  = structure1.add_standard standard
+      standard2  = structure2.add_standard standard.to_hash
+      expect(standard1).to eq standard
+      expect(standard2).to eq standard
+    end
+
+    it 'appends the standard to the list of standards' do
+      structure = Structure.new
+      structure.add_standard Standard.new(standard: 'a')
+      structure.add_standard Standard.new(standard: 'b')
+      expect(structure.standards.map &:standard).to eq %w[a b]
+    end
+
+    context 'when an id is provided' do
+      it 'does not set the id' do
+        structure = Structure.new
+        expect(structure.add_standard(id: 12).id).to eq 12
+      end
+
+      it 'blows up if there is overlap between ids' do
+        structure = Structure.new
+        structure.add_standard(id: 10)
+        structure.add_standard(id: 11)             # no overlap
+        expect { structure.add_standard(id: 10) }  # overlap
+          .to raise_error ArgumentError, /overlap/
+      end
+    end
+
+    context 'when an id is not provided' do
+      it 'sets the id to 1 if there are no standards' do
+        expect(Structure.new.add_standard({}).id).to eq 1
+      end
+
+      it 'sets the id to be one more than the max of its standards ids' do
+        structure = Structure.new [{id: 100}]
+        expect(structure.add_standard({}).id).to eq 101
+      end
+    end
+  end
+
+  describe 'equality' do
+    it 'is not equal to non-instances of Structure' do
+      expect(Structure.new []).to_not eq nil
+      expect(Structure.new []).to_not eq 123
+      expect(Structure.new []).to_not eq Hash.new
+      expect(Structure.new []).to_not eq Array.new
+    end
+
+    it 'is equal when the array of standards is equal' do
+      expect(Structure.new []       ).to     eq Structure.new([])
+      expect(Structure.new [{id: 1}]).to     eq Structure.new([{id: 1}])
+      expect(Structure.new [{id: 1}]).to_not eq Structure.new([{id: 2}])
+    end
+  end
+end
