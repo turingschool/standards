b4006390c797c2c433f10b63810a767542d1df52
Josh Cheek <josh.cheek@gmail.com>
2014-09-05 08:46:55 -0600

    Refactorings in Binary

--- lib/standards/binary.rb
+++ lib/standards/binary.rb
@@ -6,21 +6,20 @@ module Standards
     STANDARD_DATA_FILENAME = "standards.json"
 
     def self.call(argv, stdin, stdout, stderr)
-      structure      = Persistence.load STANDARD_DATA_FILENAME
+      structure = Persistence.load STANDARD_DATA_FILENAME
       command, *args = argv
 
-      if argv.first == 'add'
+      case command
+      when 'add'
         standard, *tags = args
-        standard = structure.add_standard standard: standard,
-                                          tags:     tags,
-                                          id:       1
+        standard = structure.add_standard standard: standard, tags: tags, id: 1
         Persistence.dump STANDARD_DATA_FILENAME, structure
         stdout.puts standard.to_json
-      elsif argv.first == 'select'
-        filter             = ParseSelect.call args
+      when 'select'
+        filter = ParseSelect.call(args)
         selected_standards = structure.select_standards &filter
         stdout.puts selected_standards.map(&:to_hash).to_json
-      elsif argv.first == 'generate'
+      when 'generate'
         stdout.puts GenerateBasicSite.call(structure)
       else
         raise "wat? #{argv.inspect}"
