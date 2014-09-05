a8d248d2f4fb64b4d90f03ad780e3a791e23c1db
Josh Cheek <josh.cheek@gmail.com>
2014-09-05 10:54:18 -0600

    Extract html generation of standards from rest of page

--- lib/standards/generate_basic_site.rb
+++ lib/standards/generate_basic_site.rb
@@ -9,39 +9,42 @@ module Standards
     end
 
     def call
-      @rendered ||= render
+      @rendered ||= page_html
     end
 
     private
 
     attr_accessor :structure
 
-    def render
-<<TEMPLATE
-<!doctype html>
-<html lang="en">
-  <head>
-    <title>Google</title>
-  </head>
-  <body>
-    <div class="standards">
-      #{
-structure.standards.map { |standard|
-  html = '<div class="standard">'
-  html << '<div class="id">'   << standard.id.to_s       << '</div>'
-  html << '<div class="body">' << standard.standard.to_s << '</div>'
-  html << '<div class="tags">'
-  standard.tags.each do |tag|
-    html << %'<div class="tag">#{tag}</div>'
-  end
-  html << '</div>'
-  html << '</div>'
-}.join("\n")
-}
-    </div>
-  </body>
-</html>
-TEMPLATE
+    def page_html
+      <<-HTML
+        <!doctype html>
+        <html lang="en">
+          <head>
+            <title>Google</title>
+          </head>
+          <body>
+            #{standards_html}
+          </body>
+        </html>
+      HTML
+    end
+
+    def standards_html
+      html =  '<div class="standards">'
+      html << '<div class="standard">'
+      structure.standards.map { |standard|
+        html << '<div class="id">'   << standard.id.to_s       << '</div>'
+        html << '<div class="body">' << standard.standard.to_s << '</div>'
+        html << '<div class="tags">'
+        standard.tags.each do |tag|
+          html << %'<div class="tag">#{tag}</div>'
+        end
+        html << '</div>'
+        html << '</div>'
+      }.join("\n")
+      html << '</div>'
+      html << '</div>'
     end
   end
 end
