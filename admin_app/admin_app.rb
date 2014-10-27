e0923b2cf75cfbf8acb97abd63b05325fddf1990
Josh Cheek <josh.cheek@gmail.com>
2014-10-27 00:52:40 -0600

    Add more hierarchies so its more interesting to play with

--- admin_app/admin_app.rb
+++ admin_app/admin_app.rb
@@ -13,6 +13,11 @@ class AdminApp < Sinatra::Base
     h2 = @structure.add_hierarchy name: 'h2', parent_id: h1.id
     h3 = @structure.add_hierarchy name: 'h3', parent_id: h2.id
     h4 = @structure.add_hierarchy name: 'h4', parent_id: h1.id
+    h5 = @structure.add_hierarchy name: 'h5', parent_id: h4.id
+    h6 = @structure.add_hierarchy name: 'h6', parent_id: h4.id
+    h7 = @structure.add_hierarchy name: 'h7', parent_id: h1.id
+    h8 = @structure.add_hierarchy name: 'h8', parent_id: h7.id
+    h9 = @structure.add_hierarchy name: 'h9', parent_id: h7.id
     @structure.to_json
   end
 end
